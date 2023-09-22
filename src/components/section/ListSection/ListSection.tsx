5320403ad17c269c181b2b8c6ab6dc806273717e
xenomech <gokulsmenon227@gmail.com>
2023-09-23 00:13:52 +0530

    feat: add filter feature (Partial)

--- src/components/section/ListSection/ListSection.tsx
+++ src/components/section/ListSection/ListSection.tsx
@@ -1,22 +1,54 @@
 'use client';
 import ListCard from '@/components/list/ListCard';
 import { FrontMatterType } from '@/lib/common';
+import { useEffect, useState } from 'react';
 type ListSectionType = {
   data: FrontMatterType[];
   type: 'blog' | 'snippets';
 };
 export const ListSection = ({ data, type }: ListSectionType) => {
-  const curatedLang = data.map((item) => item.language).flat(Infinity);
+  const [renderList, setRenderList] = useState<FrontMatterType[]>(data);
+  const [filter, setFilter] = useState<string[]>([]);
+
+  const curatedLang = data
+    .map((item) => item.language)
+    .flat(Infinity) as string[];
   const filterOptions = curatedLang.filter(
     (item, idx) => curatedLang.indexOf(item) === idx
   );
-  console.log(filterOptions, type);
+  useEffect(() => {
+    if (filter.length > 0) {
+      setRenderList(
+        data.filter((item) =>
+          item.language.some((item) => filter.indexOf(item) !== -1)
+        )
+      );
+    } else {
+      setRenderList(data);
+    }
+  }, [data, filter]);
 
   return (
     <div className="posts dash w-full rounded-lg border-[1px] border-black border-opacity-10 p-4 md:w-3/4">
-      {/* {type === 'blog' && <div>{JSON.stringify(filterOptions)}</div>} */}
+      <div className="item-center flex gap-2">
+        {filterOptions.map((item, idx) => (
+          <div
+            key={idx}
+            className="cursor-pointer  px-2"
+            onClick={() =>
+              setFilter(
+                filter.includes(item)
+                  ? filter.filter((element) => element !== item)
+                  : [...filter, item]
+              )
+            }
+          >
+            {item}
+          </div>
+        ))}
+      </div>
       <div>
-        {data.map((item) => (
+        {renderList.map((item) => (
           <ListCard
             title={item.title}
             slug={item.slug}
