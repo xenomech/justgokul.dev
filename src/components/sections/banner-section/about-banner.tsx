69dfee8d6b65dc72e5d7f21ad9eb71437960beef
xenomech <gokulsmenon227@gmail.com>
2023-12-18 00:05:50 +0530

    feat: redesign about page

--- src/components/sections/banner-section/about-banner.tsx
+++ src/components/sections/banner-section/about-banner.tsx
@@ -1,13 +1,22 @@
 'use client';
+import { EMOJI_LIST } from '@/assets/emoji';
 import { ArrowIcon } from '@/assets/icons';
 import { Button } from '@/components/button';
+import { GH_URL, GetGitProfile } from '@/lib/api';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import useSWR from 'swr';
+import { SocialsSection } from '../socials-section';
 
 export const AboutBanner = () => {
+  const { data, isLoading } = useSWR(GH_URL, (input) => {
+    return GetGitProfile(input);
+  });
+  console.log(data);
   const router = useRouter();
   return (
-    <section className="mt-9 flex w-full flex-col items-start justify-center gap-8 xl:mx-56">
-      <div className="flex flex-col items-start justify-start gap-6 ">
+    <section className="flex w-full flex-col items-start justify-center max-w-4xl mx-auto p-2 mt-4 rounded-lg dash gap-8">
+      <div className="flex flex-col items-start justify-start gap-6 relative h-60 bg-orange-300/60 w-full p-4 rounded-md">
         <Button
           className="flex items-center justify-center gap-2"
           variant="navigator"
@@ -16,13 +25,43 @@ export const AboutBanner = () => {
           <ArrowIcon className="h-4 w-4 rotate-180" />
           <p>Go Back</p>
         </Button>
-        <h1 className="heading">About</h1>
-        <p className="subheading max-w-2xl">
+        <FancyEmojiBG />
+        <div className="absolute p-2 bg-base-100 -bottom-20 rounded-full left-6">
+          <div className="relative w-36 h-36">
+            <Image
+              src={data?.avatar_url}
+              alt=""
+              fill
+              className="bg-cover rounded-full"
+            />
+          </div>
+        </div>
+        {/* <h1 className="heading">About</h1> */}
+        {/* <p className="subheading max-w-2xl">
           Hey, I&apos;m Gokul Suresh, a software engineer from Kochi, Kerala!
           🖥️💡 Music lover, distro hopper, exploring tech and everything in
           between.🚀 #SoftwareEngineer #TechEnthusiast #Freelancer
-        </p>
+        </p> */}
+      </div>
+      <div className="mt-20">
+        <SocialsSection />
       </div>
     </section>
+  );
+};
+
+const FancyEmojiBG = () => {
+  return (
+    <>
+      <div className="absolute top-0 left-0 right-0 bottom-0">
+        <div className="flex justify-center items-center flex-wrap gap-2">
+          {EMOJI_LIST.map((item) => (
+            <span className="text-2xl" key={item}>
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
